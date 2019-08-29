580e3aed962ef0ceeb0a9804b4b0142febe97a32
kakachake <282281884@qq.com>
2019-08-29 17:50:08 +0800

    learn

---
+++ day1/src/learn2/Demo01Method.java
@@ -0,0 +1,13 @@
+package learn2;
+
+public class Demo01Method {
+    public static void main(String[] args) {
+        printMethod();
+    }
+
+    public static void printMethod() {
+        for (int i = 0; i < 5; i++) {
+            System.out.println("*");
+        }
+    }
+}
