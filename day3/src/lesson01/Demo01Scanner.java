ab3fcf0846d1ec0bc23c7b06fb858ad861f8096f
kakachake <282281884@qq.com>
2019-09-01 00:08:39 +0800

    learn

---
+++ day3/src/lesson01/Demo01Scanner.java
@@ -0,0 +1,27 @@
+package lesson01;
+
+
+/*
+* Scanner 类的功能，可以实现键盘输入数据到程序当中
+*
+* 引用类型的一般使用步骤
+*
+* 1.导包
+*  import 包路径.类名称
+*  如果使用的目标类，和当前的类位于同一个包下，则可以省略包语句不写
+*  只有java.lang包下的内容不需要导包，其他的包都需要import
+*
+* 2.创建
+* 类名称 对象名 = new 类名称()
+*
+* 3.使用
+* 对象名.成员方法名()
+* */
+public class Demo01Scanner {
+
+    public static void main(String[] args) {
+
+    }
+
+
+}
